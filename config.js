7b279557f6e3bec4359e429d0ec21aee1b211b45
ssskopss <singhalsoven@hotmail.com>
2023-06-19 18:57:47 +0530

    added sdk file

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyD_4k1yT24K-zLUaYTrhYCd3zaUqjYnuXc",
+  authDomain: "team-voting-c6d3b.firebaseapp.com",
+  databaseURL: "https://team-voting-c6d3b-default-rtdb.firebaseio.com",
+  projectId: "team-voting-c6d3b",
+  storageBucket: "team-voting-c6d3b.appspot.com",
+  messagingSenderId: "1055384060494",
+  appId: "1:1055384060494:web:f80a9f5c19410f5e4eb91f"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
